d8c91fcc26b1b6614d39f4d3d9cdc66b6c0e3797
decheverri <decheverri@9ad2bc1c-3669-4cc8-8f04-75eb77d0e08c>
2010-08-30 18:17:35 +0000

    Moved modernizr into it's own repository.

---
+++ if-ws-html5/Main.fs
@@ -0,0 +1,18 @@
+﻿// $begin{copyright}
+//
+// This file is confidential and proprietary.
+//
+// Copyright (c) IntelliFactory, 2004-2010.
+//
+// All rights reserved.  Reproduction or use in whole or in part is
+// prohibited without the written consent of the copyright holder.
+//-----------------------------------------------------------------
+// $end{copyright}
+
+namespace IntelliFactory.WebSharper.HTML5.Main
+
+module Main =
+    open IntelliFactory.WebSharper.InterfaceGenerator
+
+    do Compiler.Compile stdout IntelliFactory.WebSharper.Html5.Modernizr.Assembly
+
