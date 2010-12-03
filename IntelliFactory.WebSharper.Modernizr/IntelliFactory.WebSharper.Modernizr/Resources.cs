873cc92236f13274ec0bebadd446e060fb939636
decheverri <decheverri@9ad2bc1c-3669-4cc8-8f04-75eb77d0e08c>
2010-12-03 23:33:11 +0000

    Missing files. Fixed doc project.

---
+++ IntelliFactory.WebSharper.Modernizr/IntelliFactory.WebSharper.Modernizr/Resources.cs
@@ -0,0 +1,10 @@
+using System.Web.UI;
+using IntelliFactory.WebSharper;
+
+namespace IntelliFactory.WebSharper.Modernizr.Resources { 
+ 	
+    public class Modernizr : IntelliFactory.WebSharper.Resources.BaseResource { 
+ 	    public Modernizr() : base("modernizr-1.6.min.js") {} 
+    } 
+ 
+}
